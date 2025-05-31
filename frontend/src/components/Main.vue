3d6a9d161a0e1c1b995bbec61bdb4371c88017c9
BirikyC <adam.ceglarz18@gmail.com>
2025-05-31 21:50:05 +0200

    Landing Page Image Style

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,13 +1,22 @@
 <template>
     <main>
-        
+        <div class="main__image">
+            <img :src="landing_image" alt="Zdjecie jachtu">
+        </div>
     </main>
 </template>
 
 <script lang="ts">
+import landing_image from '../assets/landing_image.png'
+
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: "Main"
+    name: "Main",
+    setup() {
+    return {
+      landing_image
+    };
+  }
 })
 </script>
