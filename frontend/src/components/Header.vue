139c6623b5810845a0d15b038be47a45c2e76ff5
BirikyC <adam.ceglarz18@gmail.com>
2025-05-31 03:07:37 +0200

    Header logo setup

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,9 +1,14 @@
 <template>
   <header>
-    <div></div>
+    <div class="header-logo">
+      <div class="header-logo__image">
+        <img :src="logo" alt="Sapore Celeste Logo">
+      </div>
+    </div>
   </header>
 </template>
 
 <script setup lang="ts">
+import logo from '../assets/logo.png'
 
 </script>
