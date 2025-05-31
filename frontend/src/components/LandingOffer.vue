98f8168b067c499ad616a0d735c9d5f55156d69f
BirikyC <adam.ceglarz18@gmail.com>
2025-05-31 23:34:38 +0200

    hotfix types

--- frontend/src/components/LandingOffer.vue
+++ frontend/src/components/LandingOffer.vue
@@ -1,11 +1,15 @@
 <template>
-  <li class="main-offers-list-option"></li>
+  <li class="main-offers-list-option">{{ name }}</li>
 </template>
 
 <script setup lang="ts">
-defineProps({
-  name: String,
-  price: Number,
-  discount: Number
-})
+import { defineProps } from 'vue'
+
+interface Offer{
+  name: string;
+  price: number;
+  discount: number;
+}
+
+const props = defineProps<Offer>();
 </script>
--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -4,31 +4,44 @@
             <img :src="landing_image" alt="Zdjecie jachtu">
         </div>
         <div class="main-offers">
-            <ul class="main-offers-list"></ul>
+            <ul class="main-offers-list">
+                <LandingOffer
+                    v-for="offer in offers" :key="offer.id" 
+                    :name="offer.name" 
+                    :price="offer.price" 
+                    :discount="offer.discount" >
+                </LandingOffer>
+            </ul>
         </div>
     </main>
 </template>
 
-<script lang="ts">
-import { defineComponent, onMounted } from 'vue';
-
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
 import landing_image from '../assets/landing_image.png'
 
-export default defineComponent({
-    name: "Main",
-    setup() {
-        onMounted(async () => {
-            try{
-                const response = await fetch("http://localhost:6969/offers/get");
-                const data = await response.json()
-            } catch (err){
-                console.error("Error: ", err);
-            }
-        });
+import LandingOffer from './LandingOffer.vue';
 
-        return {
-            landing_image
-        };
+interface Offer{
+  id: number;
+  name: string;
+  price: number;
+  discount: number;
+}
+
+const offers = ref<Offer[]>([])
+
+async function get_offers(){
+    try{
+        const response = await fetch("http://localhost:6969/offers/get");
+        const data = await response.json();
+        offers.value = data;
+    } catch (err){
+        console.error("Error: ", err);
     }
+}
+
+onMounted(() => {
+    get_offers();
 })
 </script>
